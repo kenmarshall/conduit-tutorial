9e7a829d016c394a0c78b558646f54c63d6c799e
Kenneth Marshall <kdmarshall89@gmail.com>
2016-11-12 11:36:29 -0500

    created settings template

--- src/js/auth/auth.controller.js
+++ src/js/auth/auth.controller.js
@@ -5,6 +5,7 @@ class AuthCtrl {
 		this._User = User;
 		this.title = $state.current.title;
 		this.authType = $state.current.name.replace('app.', '');
+		this._$state = $state;
 	}
 
 	submitForm (){
@@ -13,14 +14,12 @@ class AuthCtrl {
 		this._User.attemptAuth(this.authType, this.formData).then(
 
 			(res) => {
-				this.isSubmitting = false;
-				console.log(res);
+				this._$state.go('app.home');
 			},
 
 			(err) => {
 				this.isSubmitting = false
-				this.errors = err.data.errors;
-				console.log(err.data.errors);
+				this.errors = err.data.errors;				
 			}
 		);
 	
